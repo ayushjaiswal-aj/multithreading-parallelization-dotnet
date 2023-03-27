2044c5327e5e6cdf05566498f0c835df40aa2ef8
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2023-03-27 10:49:15 +0530

    First commit

---
+++ TaskProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace TaskProgramming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //TaskCreation.CreateAndStartSimpleTasks();
+            TaskCreation.TasksWithReturnValues();
+
+            Console.WriteLine("Main program done, press any key.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TaskProgramming/TaskCancellation.cs
@@ -0,0 +1,7 @@
+namespace TaskProgramming
+{
+    public class TaskCancellation
+    {
+        
+    }
+}
